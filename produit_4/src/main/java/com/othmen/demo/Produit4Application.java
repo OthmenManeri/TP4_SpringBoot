76b61e5596f00c135216bb66b2182a012fee0890
OthmenManeri <91779774+OthmenManeri@users.noreply.github.com>
2022-04-22 17:22:50 +0100

    Add files via upload

---
+++ produit_4/src/main/java/com/othmen/demo/Produit4Application.java
@@ -0,0 +1,13 @@
+package com.othmen.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Produit4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Produit4Application.class, args);
+	}
+
+}
---
+++ produit_4/src/test/java/com/othmen/demo/Produit4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.othmen.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Produit4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
